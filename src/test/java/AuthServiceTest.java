8634a2d4a6104889a02575b8084948cac1b8c1df
berkbogac-akgunes <berkb_akgunes@outlook.com>
2026-02-25 21:00:28 +0300

    fix test

--- src/test/java/AuthServiceTest.java
+++ src/test/java/AuthServiceTest.java
@@ -21,7 +21,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-//Testing
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
 
